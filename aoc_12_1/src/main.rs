2e9832b9fed31a8dea22d9b082fd329325e836e8
ckuenzi <ckuenzi@student.ehtz.ch>
2018-12-12 12:28:45 +0100

    Day 12

--- aoc_12_1/src/main.rs
+++ aoc_12_1/src/main.rs
@@ -1,64 +1,49 @@
-use std::collections::HashMap;
 fn main() {
+    let padding = 3;
+    let iterations = 20;
+
     let mut input = include_str!("input.txt").lines();
-    let initial_state = input
+    let mut initial_state = input
         .next()
         .unwrap()
         .split(' ')
         .nth(2)
         .unwrap()
         .chars()
-        .map(|c| c == '#')
+        .map(|c| if c == '#' { 1 } else { 0 })
         .collect::<Vec<_>>();
-    let padding = initial_state.len() * 5;
-
     input.next();
-    let rules: HashMap<_, _> = input
-        .map(|line| {
-            let mut e = line.split(' ').step_by(2);
-            (
-                e.next()
-                    .unwrap()
-                    .chars()
-                    .map(|c| c == '#')
-                    .collect::<Vec<_>>(),
-                e.next().unwrap() == "#",
-            )
+
+    let mut input = input.collect::<Vec<_>>();
+    input.sort();
+
+    let rules: Vec<_> = input
+        .iter()
+        .rev()
+        .map(|rule| match rule.chars().last().unwrap() {
+            '#' => 1,
+            _ => 0,
         })
         .collect();
 
-    let mut pots = vec![false; padding * 2];
+    let mut pots = vec![0; padding];
+    pots.append(&mut initial_state);
+    pots.append(&mut vec![0; iterations]);
+    pots.append(&mut vec![0; padding]);
 
-    initial_state
-        .iter()
-        .enumerate()
-        .for_each(|plant| pots[plant.0 + padding] = *plant.1);
-
-    let mut last = 0;
-
-    for i in 1..=200 {
-        let mut changes = vec![];
+    for i in 1..=iterations {
+        let mut rolling = 0;
         for pos in 0..pots.len() - 5 {
-            let mut res;
-            if let Some(slice) =
-                rules.get(&pots.iter().skip(pos).take(5).fold(vec![], |mut out, pot| {
-                    out.push(*pot);
-                    out
-                })) {
-                res = *slice;
-            } else {
-                res = false;
-            }
-            changes.push((pos + 2, res));
+            rolling = ((rolling << 1) & 31) + pots[pos + 4]; //<----
+            pots[pos + 2] = rules[rolling];
         }
-        changes.iter().for_each(|change| pots[change.0] = change.1);
+
         let num = pots.iter().enumerate().fold(0, |mut out, pot| {
-            if *pot.1 {
+            if *pot.1 == 1 {
                 out += pot.0 as i32 - padding as i32;
             }
             out
         });
-        println!("{} -> {} -> {}", i, num, num - last);
-        last = num;
+        println!("{} -> {}", i, num)
     }
 }
