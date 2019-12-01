e633f49b35832cd88fbc51b56accc4e89d5eb3d7
ckuenzi <ckuenzi@student.ee.ethz.ch>
2019-12-01 11:18:24 +0100

    just git stuff

--- aoc_2018/aoc_24_2/src/main.rs
+++ aoc_2018/aoc_24_2/src/main.rs
@@ -3,67 +3,85 @@ use std::cmp::Reverse;
 fn main() {
     let input = include_str!("input.txt");
     let armies: Vec<_> = input.split("\r\n\r\n").collect();
-    let mut groups: Vec<Group> = armies[0].lines().skip(1).map(|l| parse(l, true)).collect();
+    let mut groups0: Vec<Group> = armies[0].lines().skip(1).map(|l| parse(l, true)).collect();
     armies[1]
         .lines()
         .skip(1)
-        .for_each(|l| groups.push(parse(l, false)));
+        .for_each(|l| groups0.push(parse(l, false)));
 
-    loop {
-        let mut attackers: Vec<_> = (0..groups.len()).collect();
-        attackers.sort_by_key(|&i| Reverse((groups[i].ep(), groups[i].init)));
-        let mut attacks: Vec<(usize, usize)> = vec![];
-        for a in attackers {
-            let mut defenders: Vec<_> = (0..groups.len())
-                .filter(|&d| !groups[d].is_defending && groups[a].team != groups[d].team)
-                .collect();
-            defenders.sort_by_key(|&d| {
-                Reverse((groups[a].damage(&groups[d]), groups[d].ep(), groups[d].init))
-            });
-            if defenders.is_empty() {
-                continue;
-            }
-            let d = defenders[0];
-            if groups[a].damage(&groups[d]) > 0 {
-                attacks.push((a, d));
-                groups[d].is_defending = true;
-            }
-        }
-
-        attacks.sort_by_key(|&t| Reverse(groups[t.0].init));
-
-        for (a, d) in attacks {
-            if groups[a].units <= 0 {
-                continue;
-            }
-            groups[d].units -= groups[a].damage(&groups[d]) / groups[d].hp;
-        }
-
-        let mut army1_alive = false;
-        let mut army2_alive = false;
+    let mut boost = 0;
+    'boost: loop {
+        let mut groups = groups0.clone();
+        boost += 1;
         for i in 0..groups.len() {
-            groups[i].is_defending = false;
             if groups[i].team {
-                army1_alive = true;
-            } else {
-                army2_alive = true;
+                groups[i].ap += boost;
             }
         }
-        groups = groups
-            .into_iter()
-            .filter(|g| g.units >= 0)
-            .collect::<Vec<Group>>();
+        'battle: loop {
+            let mut total_killed = 0;
+            let mut attackers: Vec<_> = (0..groups.len()).collect();
+            attackers.sort_by_key(|&i| Reverse((groups[i].ep(), groups[i].init)));
+            let mut attacks: Vec<(usize, usize)> = vec![];
+            for a in attackers {
+                let mut defenders: Vec<_> = (0..groups.len())
+                    .filter(|&d| !groups[d].is_defending && groups[a].team != groups[d].team)
+                    .collect();
+                defenders.sort_by_key(|&d| {
+                    Reverse((groups[a].damage(&groups[d]), groups[d].ep(), groups[d].init))
+                });
+                if defenders.is_empty() {
+                    continue;
+                }
+                let d = defenders[0];
+                if groups[a].damage(&groups[d]) > 0 {
+                    attacks.push((a, d));
+                    groups[d].is_defending = true;
+                }
+            }
 
-        if !army1_alive || !army2_alive {
-            break;
+            attacks.sort_by_key(|&t| Reverse(groups[t.0].init));
+
+            for (a, d) in attacks {
+                if groups[a].units <= 0 {
+                    continue;
+                }
+                groups[d].units -= groups[a].damage(&groups[d]) / groups[d].hp;
+                total_killed += groups[a].damage(&groups[d]) / groups[d].hp;
+            }
+
+            let mut army1_alive = false;
+            let mut army2_alive = false;
+            for i in 0..groups.len() {
+                groups[i].is_defending = false;
+                if groups[i].team {
+                    army1_alive = true;
+                } else {
+                    army2_alive = true;
+                }
+            }
+            groups = groups
+                .into_iter()
+                .filter(|g| g.units >= 0)
+                .collect::<Vec<Group>>();
+
+            if !army1_alive && army2_alive {
+                break 'battle;
+            }
+            if army1_alive && !army2_alive {
+                let result = groups.iter().fold(0, |mut out, g| {
+                    out += g.units;
+                    out
+                });
+                println!("Boost: {}", boost);
+                println!("Units: {}", result);
+                break 'boost;
+            }
+            if total_killed == 0 {
+                break 'battle;
+            }
         }
     }
-
-    let result = groups.iter().fold(0, |mut out, g| {
-        out += g.units;
-        out
-    });
-    println!("{:#?}", result);
 }
 
 fn parse_damage(text: &str) -> Damage {
