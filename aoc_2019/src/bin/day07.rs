087738ee1f14feedf8b70060fb3884e012c6b747
ckuenzi <ckuenzi@student.ee.ethz.ch>
2019-12-08 01:03:58 +0100

    Day 07 simplifications

--- aoc_2019/src/bin/day07.rs
+++ aoc_2019/src/bin/day07.rs
@@ -6,7 +6,7 @@ fn main() {
     let input = intscript::read_intscript_from_file(include_str!("inputs\\day07a.txt"));
     let mut computers = vec![intscript::Computer::new(input.clone()); 5];
 
-    let mut res = 0;
+    let mut part1 = 0;
     for phases in (0..5).permutations(5) {
         let mut next_input = 0;
         for (i, computer) in computers.iter_mut().enumerate() {
@@ -14,25 +14,24 @@ fn main() {
             next_input = computer.siso(next_input);
             computer.reset();
         }
-        res = cmp::max(res, next_input);
+        part1 = cmp::max(part1, next_input);
     }
-    println!("{}", res);
 
-    //part2
+    let mut part2 = 0;
     for phases in (5..10).permutations(5) {
-        let mut start_value = 0;
+        let mut next_input = 0;
         for (i, computer) in computers.iter_mut().enumerate() {
             computer.add_input(phases[i]);
         }
         while !computers[4].halted {
             for computer in computers.iter_mut() {
-                start_value = computer.siso(start_value);
-                res = cmp::max(res, start_value);
+                next_input = computer.siso(next_input);
+                part2 = cmp::max(part2, next_input);
             }
         }
         for computer in computers.iter_mut() {
             computer.reset();
         }
     }
-    println!("{}", res);
+    println!("{}\n{}", part1, part2);
 }
