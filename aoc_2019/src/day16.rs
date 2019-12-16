a92cf4a680cef51c018abea5495ed5106dcf2aef
ckuenzi <ckuenzi@student.ethz.ch>
2019-12-16 16:18:42 +0100

    Day 16 double speed

--- aoc_2019/src/day16.rs
+++ aoc_2019/src/day16.rs
@@ -1,18 +1,18 @@
 #[aoc_generator(day16)]
-fn gen(input: &str) -> Vec<i8> {
+fn gen(input: &str) -> Vec<u8> {
     input
         .chars()
-        .map(|x| x.to_digit(10).unwrap() as i8)
+        .map(|x| x.to_digit(10).unwrap() as u8)
         .collect()
 }
 
 #[aoc(day16, part1)]
-fn part1(signal: &Vec<i8>) -> usize {
+fn part1(signal: &Vec<u8>) -> usize {
     range_to_num(&fft(signal.to_vec(), 100), 0, 8)
 }
 
 #[aoc(day16, part2)]
-fn part2(signal: &Vec<i8>) -> usize {
+fn part2(signal: &Vec<u8>) -> usize {
     let mut big_signal = vec![];
     for _ in 0..10000 {
         big_signal.append(&mut signal.clone());
@@ -21,17 +21,19 @@ fn part2(signal: &Vec<i8>) -> usize {
     range_to_num(&ffft(big_signal, 100, offset), offset, offset + 8)
 }
 
-fn ffft(mut signal: Vec<i8>, cycles: u32, offset: usize) -> Vec<i8> {
+fn ffft(mut signal: Vec<u8>, cycles: u32, offset: usize) -> Vec<u8> {
     for _ in 0..cycles {
+        let mut prev = signal[signal.len() - 1];
         for pos in (offset..signal.len() - 1).into_iter().rev() {
-            signal[pos] = (signal[pos + 1] + signal[pos]) % 10;
+            prev = (prev + signal[pos]) % 10;
+            signal[pos] = prev;
         }
     }
     signal
 }
 
-fn fft(mut signal: Vec<i8>, cycles: u32) -> Vec<i8> {
-    let mut new_signal: Vec<i8> = vec![];
+fn fft(mut signal: Vec<u8>, cycles: u32) -> Vec<u8> {
+    let mut new_signal: Vec<u8> = vec![];
     let mut patterns = vec![];
     for i in 1..=signal.len() {
         patterns.push(pattern(i));
@@ -39,13 +41,13 @@ fn fft(mut signal: Vec<i8>, cycles: u32) -> Vec<i8> {
     for _ in 0..cycles {
         for i in 0..signal.len() {
             new_signal.push(
-                signal
+                (signal
                     .iter()
                     .zip(patterns[i].iter().cycle().skip(1))
-                    .map(|(&s, &p)| s * p)
+                    .map(|(&s, &p)| s as i8 * p)
                     .sum::<i8>()
                     .abs()
-                    % 10,
+                    % 10) as u8,
             )
         }
         signal = new_signal;
@@ -62,7 +64,7 @@ fn pattern(reps: usize) -> Vec<i8> {
     output
 }
 
-fn range_to_num(signal: &Vec<i8>, start: usize, end: usize) -> usize {
+fn range_to_num(signal: &Vec<u8>, start: usize, end: usize) -> usize {
     signal
         .iter()
         .skip(start)
