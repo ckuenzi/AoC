4290857037f7ece3a08b8c066d60b93620259c4e
ckuenzi <ckuenzi@student.ethz.ch>
2019-12-19 11:46:32 +0100

    Day 19 simplifications

--- aoc_2019/src/day19.rs
+++ aoc_2019/src/day19.rs
@@ -1,13 +1,12 @@
 use super::intscript::Computer;
+use itertools::Itertools;
 
 #[aoc(day19, part1)]
 fn part1(input: &str) -> i64 {
     let mut computer = Computer::new_from_text(input);
     let mut sum = 0;
-    for y in 0..50 {
-        for x in 0..50 {
-            sum += check_beam(x, y, &mut computer) as i64;
-        }
+    for (x, y) in (0..50).into_iter().cartesian_product((0..50).into_iter()) {
+        sum += check_beam(x, y, &mut computer) as i64;
     }
     sum
 }
@@ -15,26 +14,16 @@ fn part1(input: &str) -> i64 {
 #[aoc(day19, part2)]
 fn part2(input: &str) -> i64 {
     let mut computer = Computer::new_from_text(input);
-    let mut left = 3;
-    let mut line = 3;
-    loop {
-        for l in (left + 1)..(left + 3) {
-            if check_beam(l, line, &mut computer) {
-                left = l;
-                break;
-            }
-        }
-        if line > 100
-            && check_beam(left + 99, line, &mut computer)
-            && check_beam(left, line - 99, &mut computer)
-            && check_beam(left + 99, line - 99, &mut computer)
-        {
-            break;
-        }
-        line += 1;
+    let (mut x, mut y) = (3, 3);
+    while !(y > 100 && check_beam(x + 99, y - 99, &mut computer)) {
+        y += 1;
+        while {
+            x += 1;
+            !check_beam(x, y, &mut computer)
+        } {}
     }
 
-    left * 10000 + (line - 99)
+    x * 10000 + (y - 99)
 }
 
 fn check_beam(x: i64, y: i64, computer: &mut Computer) -> bool {
